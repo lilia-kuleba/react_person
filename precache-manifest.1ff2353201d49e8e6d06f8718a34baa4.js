cc7bf963d8356869f494a1a2f6d4c82ee06442be
Liliia Kuleba <kuleba.lilia.1872003@gmail.com>
2021-10-08 17:57:35 +0300

    Updates

---
+++ precache-manifest.1ff2353201d49e8e6d06f8718a34baa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2ae192f89f50db9c19bdcc36ddc4b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e49537ff3213162c9325",
+    "url": "./static/css/main.4f788214.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49537ff3213162c9325",
+    "url": "./static/js/main.b16a2f86.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
